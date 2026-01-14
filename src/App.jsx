a83838cd7781a59099a069c4c825f09c6a60d3c6
Yasser Algburi <yasseralgburi@gmail.com>
2026-01-13 20:37:10 -0500

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="bg-blue-500 text-white p-4">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,11 +25,11 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="bg-blue-500 text-white p-4">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
